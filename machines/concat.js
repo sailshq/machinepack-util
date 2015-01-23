bae8d6da528960bd531b86e2bc355989cbedb126
Mike McNeil <mike@balderdash.co>
2015-01-22 23:19:37 -0800

    Fixed input example.

--- machines/concat.js
+++ machines/concat.js
@@ -3,7 +3,11 @@ module.exports = {
   description: 'Concatenate an array of strings and return the combined version.',
   extendedDescription: '',
   inputs: {
-    strings: ['foo']
+    strings: {
+      description: 'The array of strings to concatenate',
+      example: ['foo'],
+      required: true
+    }
   },
   defaultExit: 'success',
   exits: {
