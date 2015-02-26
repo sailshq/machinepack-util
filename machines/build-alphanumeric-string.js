51e7f47a683a6d1779404bbd6f854431ec885ace
Mike McNeil <mike@balderdash.co>
2015-02-25 17:38:02 -0800

    build-alphanumeric-string

---
+++ machines/build-alphanumeric-string.js
@@ -0,0 +1,51 @@
+module.exports = {
+
+
+  friendlyName: 'Build alphanumeric string',
+
+
+  description: 'Build a random alphanumeric string.  All alphabetical characters will be upper-case.',
+
+
+  extendedDescription: '',
+
+
+  inputs: {
+
+    numChars: {
+      description: 'The number of random alphanumeric characters to generate.',
+      example: 6,
+      defaultsTo: 6
+    }
+
+  },
+
+
+  defaultExit: 'success',
+
+
+  exits: {
+
+    error: {
+      description: 'Unexpected error occurred.',
+    },
+
+    success: {
+      description: 'Done.',
+      example: '6WE4RT'
+    },
+
+  },
+
+
+  fn: function (inputs,exits) {
+    var rdmString = '';
+    for( var i=0; i < (inputs.numChars||6); i++) {
+      rdmString += Math.random().toString(36).substr(2).toUpperCase();
+    }
+    return exits.success(rdmString.substr(0, inputs.numChars||6));
+  },
+
+
+
+};
