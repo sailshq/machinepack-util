76a6376589b150dbbb525ca23876bfa6abc2dc79
Mike McNeil <mike@balderdash.co>
2015-03-16 21:30:58 -0700

    Fix is-it-defined.

--- machines/is-it-defined.js
+++ machines/is-it-defined.js
@@ -1,36 +1,51 @@
 module.exports = {
-  "friendlyName": "Is it defined?",
-  "inputs": {
-    "value": {
-      "extendedDescription": "A value of any type may be provided.  If it evaluates to \"undefined\", the \"fail\" exit will be triggered.  Otherwise the \"pass\" exit will be triggered.",
-      "friendlyName": "value",
-      "description": "The value to check.",
-      "typeclass": "*"
+
+
+  friendlyName: 'Is it defined?',
+
+
+  inputs: {
+
+    value: {
+      extendedDescription: 'A value of any type may be provided.  If it evaluates to \'undefined\', the \'fail\' exit will be triggered.  Otherwise the \'pass\' exit will be triggered.',
+      friendlyName: 'value',
+      description: 'The value to check.',
+      typeclass: '*'
     }
+
   },
-  "exits": {
-    "error": {
-      "example": undefined
+
+
+  exits: {
+
+    error: {
+      description: 'Unexpected error occurred.'
     },
-    "success": {
-      "id": "success",
-      "friendlyName": "pass",
-      "description": "The value is defined.",
-      "getExample": function(inputs, env, input) {
+
+    success: {
+      friendlyName: 'pass',
+      description: 'The value is defined.',
+      getExample: function(inputs, env, input) {
         return inputs.value;
       }
     },
-    "fail": {
-      "friendlyName": "fail",
-      "description": "The value is undefined."
+
+    fail: {
+      friendlyName: 'fail',
+      description: 'The value is undefined.'
     }
+
   },
-  "defaultExit": "success",
-  "fn": function(inputs, exits, env) {
+
+
+  'defaultExit': 'success',
+
+
+  'fn': function(inputs, exits, env) {
     if (typeof(inputs.value) === 'undefined') {
       return exits.fail();
     }
     return exits.success(inputs.value);
-  },
-  "identity": "Isitdefined_question"
+  }
+
 };
