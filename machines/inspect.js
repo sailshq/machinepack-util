f80734cb0c5d5380aa608583b696a97807c2991d
Scott Gress <scottmgress@gmail.com>
2016-08-02 14:33:11 -0500

    Add comments to `inspect`

--- machines/inspect.js
+++ machines/inspect.js
@@ -35,16 +35,26 @@ module.exports = {
 
 
   fn: function(inputs, exits) {
+
+    // Import `util`.
     var util = require('util');
+
+    // Import the `isError` and `isObject` Lodash functions.
     var isError = require('lodash.iserror');
     var isObject = require('lodash.isobject');
 
+    // If the value is an error, output its stack through the `success` exit.
     if (isError(inputs.value)) {
       return exits.success(util.inspect(inputs.value.stack));
     }
+
+    // If the value is an object, output it to infinite(ish) depth through
+    // the `success` exit.
     if (isObject(inputs.value)) {
       return exits.success(util.inspect(inputs.value, {depth: null}));
     }
+
+    // Otherwise just run `inspect` on it and output through the `success` exit.
     return exits.success(util.inspect(inputs.value));
   }
 
