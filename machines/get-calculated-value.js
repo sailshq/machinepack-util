e903ec979ca427cf7daeaa6d51185fe169688d61
Scott Gress <scottmgress@gmail.com>
2016-11-07 08:36:17 -0500

    Add "Get calculated value" machine

---
+++ machines/get-calculated-value.js
@@ -0,0 +1,61 @@
+module.exports = {
+
+
+  friendlyName: 'Get calculated value',
+
+
+  description: 'Get a value calculated by an anonymous machine (a "getter").',
+
+
+  inputs: {
+
+    resultExemplar: {
+      friendlyName: 'Example result',
+      description: 'An example of what the calculated value with look like.',
+      isExemplar: true,
+      defaultsTo: '*'
+    },
+
+    getter: {
+      friendlyName: 'Getter',
+      description: 'The machine used to calculate the value.',
+      example: '->',
+      contract: {
+        inputs: {},
+        exits: {
+          success: {
+            outputFriendlyName: 'Value',
+            outputDescription: 'The calculated value.',
+            like: 'resultExemplar'
+          }
+        },
+      },
+      required: true
+    }
+
+  },
+
+
+  exits: {
+
+    success: {
+      outputFriendlyName: 'Value',
+      like: 'resultExemplar',
+      outputDescription: 'The calculated value.'
+    },
+
+  },
+
+
+  fn: function(inputs, exits) {
+
+    // Run the getter and output the value through the `success` exit
+    // unless an error occurs.
+    inputs.getter().exec({
+      success: exits.success,
+      error: exits.error
+    });
+
+  }
+
+};
